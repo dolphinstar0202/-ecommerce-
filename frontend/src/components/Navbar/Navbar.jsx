1d9a6c444ffe40e427382ea4a1f46ab7cabadd1c
dolphinstar0202 <eliasforsberger@gmail.com>
2024-06-29 13:44:05 +0530

    added searchbar functionality (alongwith use debounce concept)

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import "./Navbar.css"
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars,faMagnifyingGlass,faCartPlus,faHeart,faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import WishlistPanelContext from '../../context/WishlistPanleslider/WishlistPanelContext'
 import WishlistPanel from '../WishlistsPanel/WishlistPanel'
+import { items } from '../Data'
+import debounce from 'lodash.debounce';
 
-//created this function 'cause everytime when navigated through <Link> tag, the loaded page was loded with its middle
-//part and some times bottom part.
+
 const scrollToTop = () => {
   window.scrollTo(0, 0)
 }
 
 const Navbar = () => {
   
-  const {isOpen, setIsOpen} = useContext(WishlistPanelContext) 
+  const [ query, setQuery ] = useState("");
+  const {isOpen, setIsOpen} = useContext(WishlistPanelContext)
+
+  const filteredProd = query? items.filter((item) => item.title.toLowerCase().includes(query)) : [];
 
   const handleWishlistPanel = () => {
     setIsOpen(!isOpen);
   }
+
+  const updateQuery = (e) => setQuery(e.target.value.toLowerCase());
+  const debouncedOnchange = debounce(updateQuery, 300);
+
+  
 
   return (
     <div className='Navbar' >
@@ -30,17 +39,33 @@ const Navbar = () => {
             <li><Link onClick={scrollToTop} to='/' className='link-text' >HOME <FontAwesomeIcon icon={faAngleDown} className='icon-styling-faAngleDown' size="2xs" transform="down-2" /></Link></li>
             <li><Link onClick={scrollToTop} to='/productpage' className='link-text' >SHOP <FontAwesomeIcon icon={faAngleDown} className='icon-styling-faAngleDown' size="2xs" transform="down-2" /></Link></li>
             <li><Link onClick={scrollToTop} className='link-text' >BLOG <FontAwesomeIcon icon={faAngleDown} className='icon-styling-faAngleDown' size="2xs" transform="down-2" /></Link></li>
-            <li><Link onClick={scrollToTop} className='link-text' >PAGES <FontAwesomeIcon icon={faAngleDown} className='icon-styling-faAngleDown' size="2xs" transform="down-2" /></Link></li>
+            {/* <li><Link onClick={scrollToTop} className='link-text' >PAGES <FontAwesomeIcon icon={faAngleDown} className='icon-styling-faAngleDown' size="2xs" transform="down-2" /></Link></li> */}
             <li><Link onClick={scrollToTop} className='link-text' >ABOUT US</Link></li>
             <li><Link onClick={scrollToTop} className='link-text' >CONTACT US</Link></li>
         </ul>
       </div>
       <div className="wishcartsearchicon">
+        <div className="search-bar">
+          <input className='search-prod' type='text' onChange={debouncedOnchange} placeholder='Search...' />
+          <Link onClick={scrollToTop} className='search-icon' ><FontAwesomeIcon icon={faMagnifyingGlass} /></Link>
+          {query && (
+          <div className="suggestions-list">
+            {filteredProd.map((item) => (
+              <div key={item.id} className="suggestion-item" >
+                <Link to={`/productdetails/${item.id}`} style={{textDecoration:'none', color:'rgb(50,50,50)', display:'flex', alignItems:'center'}} >
+                <img src={item.imgURL} alt="" style={{height:'30px', width:'30px', backgroundColor:'lightgrey', marginRight:'10px', borderRadius:'2px' }} />
+                {item.title.toLowerCase()}
+                </Link>
+              </div>
+              
+            ))} 
+          </div>
+          )}
+        </div>
         <ul>
-            <li><Link onClick={handleWishlistPanel} className='link-text' ><FontAwesomeIcon icon={faHeart} className='icon-styling' /></Link></li>
-            <li><Link onClick={scrollToTop} to='/cartpage' className='link-text' ><FontAwesomeIcon icon={faCartPlus} className='icon-styling' /></Link></li>
-            <li><Link onClick={scrollToTop} className='link-text' ><FontAwesomeIcon icon={faMagnifyingGlass} className='icon-styling' /></Link></li>
-            <li><Link onClick={scrollToTop} className='link-text' ><FontAwesomeIcon icon={faBars} className='icon-styling' /></Link></li>
+            <li><Link onClick={handleWishlistPanel} ><FontAwesomeIcon icon={faHeart} className='icon-styling' /></Link></li>
+            <li><Link onClick={scrollToTop} to='/cartpage' ><FontAwesomeIcon icon={faCartPlus} className='icon-styling' /></Link></li>
+            <li><Link onClick={scrollToTop} ><FontAwesomeIcon icon={faBars} className='icon-styling' /></Link></li>
         </ul>        
       </div>
       {isOpen && <div className="overlay">
